03e8750cf2582260193377e0ce645b4438d2663d
Aradhita <157782048+Aradhita1234@users.noreply.github.com>
2025-07-17 12:51:44 +0530

    Delete ChangeString.js

--- ChangeString.js
+++
@@ -1,6 +0,0 @@
-//Change the string login to main in the given URL 
-Originalurl = "http://leaftaps.com/opentaps/control/login";
-console.log(Originalurl); // Output: http://leaftaps.com/opentaps/control/login
-// Replace 'login' with 'main'
-const Expectedurl = Originalurl.replace("login", "main");
-console.log(Expectedurl); // Output: http://leaftaps.com/opentaps/control/main
